c8f31b249af7b3bc575347311a8ba4246a46bfd2
Ubuntu <vlad.garkusha1999@gmail.com~>
2026-05-22 22:11:07 +0300

    change errors and pino

--- src/server.js
+++ src/server.js
@@ -6,8 +6,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => {
-  (pino({
+
+app.use(
+  pino({
     level: 'info',
     transport: {
       target: 'pino-pretty',
@@ -21,19 +22,22 @@ app.use((req, res, next) => {
       },
     },
   }),
-    next());
-});
+);
+
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
 });
 app.get('/notes/:noteId', (req, res) => {
-  const noteId = req.params;
+  const noteId = Number(req.params.noteId);
   res
     .status(200)
     .json({ id: noteId, message: `Retrieved note with ID: ${noteId}` });
 });
-app.use((err, req, res) => {
-  res.status(404).json({ message: 'Route not found', error: err.message });
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
 });
 app.use((err, req, res, next) => {
   const isProd = process.env.NODE_ENV === 'production';
@@ -44,9 +48,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
