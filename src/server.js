42e0bfd79a1d73ad0284114aa607a5eb94135ef2
Ubuntu <vlad.garkusha1999@gmail.com~>
2026-05-22 22:14:29 +0300

    change dinamical response id

--- src/server.js
+++ src/server.js
@@ -29,9 +29,7 @@ app.get('/notes', (req, res) => {
 });
 app.get('/notes/:noteId', (req, res) => {
   const noteId = Number(req.params.noteId);
-  res
-    .status(200)
-    .json({ id: noteId, message: `Retrieved note with ID: ${noteId}` });
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 app.get('/test-error', (req, res) => {
   throw new Error('Simulated server error');
