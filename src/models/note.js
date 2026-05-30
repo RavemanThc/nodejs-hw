6edeef9d1dd8ab7e3ed8e3bbbc2ac61488a2dde6
Ubuntu <vlad.garkusha1999@gmail.com~>
2026-05-30 21:22:59 +0300

    v_1

---
+++ src/models/note.js
@@ -0,0 +1,38 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    tag: {
+      type: String,
+      default: 'Todo',
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+export const Note = model('Note', noteSchema);
