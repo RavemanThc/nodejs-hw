e6869cce20a28d1cf61fa4fd36c482fb845741b4
RavemanThc <vlad.garkusha1999@gmail.com>
2026-06-07 12:07:17 +0300

    change folder name to constants

--- src/models/note.js
+++ src/models/note.js
@@ -1,6 +1,6 @@
 import { Schema } from 'mongoose';
 import { model } from 'mongoose';
-import { TAGS } from '../contacts/tags.js';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
