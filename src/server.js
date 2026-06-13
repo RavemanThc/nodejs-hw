ac7783579829b8fcdc9dafc478e37a66e83161b2
RavemanThc <vlad.garkusha1999@gmail.com>
2026-06-13 23:42:25 +0300

    change names

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import authRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import { errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/authRoutes.js';
