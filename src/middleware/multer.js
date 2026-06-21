3cf3764cd3933f0cea137e81e595ede5abd8a109
RavemanThc <vlad.garkusha1999@gmail.com>
2026-06-21 11:31:52 +0300

    change multer

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -5,15 +5,8 @@ export const upload = multer({
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
-  fileFilter: (req, file, cb) => {
-    const allowedTypes = [
-      'image/jpeg',
-      'image/jpg',
-      'image/png',
-      'image/gif',
-      'image/webp',
-    ];
-    if (allowedTypes.includes(file.mimetype)) {
+  ffileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
       cb(null, true);
     } else {
       cb(
